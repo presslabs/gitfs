9970ff6e1f57c65c51c3c6e83d55e3f8c8880381
vtemian <vladtemian@gmail.com>
2014-06-30 13:23:50 +0300

    Compose repo clone repos

--- gitiumFS/config.py
+++ gitiumFS/config.py
@@ -1,7 +1,12 @@
-class Config(dict):
-  def __init__(self, *args, **kwargs):
-    super(Config, self).__init__(*args, **kwargs)
+import yaml
 
+
+class Config(dict):
+  def __init__(self, path=None, *args, **kwargs):
+    if path is not None:
+      self.load(path)
+
+    super(Config, self).__init__(*args, **kwargs)
     self.__dict__ = self.build(self)
 
   def build(self, root):
@@ -10,3 +15,10 @@ class Config(dict):
         root[key] = Config(root[key])
 
     return root
+
+  def load(self, path):
+    with open(path) as f:
+      content = f.read()
+      self.update(yaml.load(content))
+
+default_config = Config("/etc/gitfs/conf.yaml")
--- gitiumFS/filesystems/git.py
+++ gitiumFS/filesystems/git.py
@@ -1,3 +1,5 @@
+import re
+from gitiumFS import default_config
 
 from .passthrough import PassthroughFuse
 
@@ -5,4 +7,9 @@ from .passthrough import PassthroughFuse
 class GitFuse(PassthroughFuse):
   def __init__(self, remote_url):
     self.remote_url = remote_url
-    self.root = "repo"
+    self.root = self._get_root()
+
+  def _get_root(self):
+    print self.remote_url
+    match = re.search(r"(?P<repo_name>[A-Za-z0-9]+)\.git", self.remote_url)
+    return "%s/%s" % (default_config.repos_path, match.group("repo_name"))
