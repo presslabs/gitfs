fdfad57e9e6fe009f7811a2593e0160a9c29c7b1
vtemian <vladtemian@gmail.com>
2014-07-01 12:56:19 +0300

    Clone the repo and mount the fs

--- examples/simple_repo.py
+++ examples/simple_repo.py
@@ -1,4 +1,10 @@
+from fuse import FUSE
+
 from gitiumFS.filesystems import GitFuse
 
-remote_url = "git@github.com:vtemian/FaceFS.git"
+
+remote_url = "git://github.com/vtemian/FaceFS.git"
 fs = GitFuse(remote_url)
+
+
+FUSE(fs, "mnt", foreground=True)
--- gitiumFS/filesystems/git.py
+++ gitiumFS/filesystems/git.py
@@ -1,6 +1,8 @@
 import re
-from gitiumFS import default_config
 
+from tamia import Repository
+
+from gitiumFS import default_config
 from .passthrough import PassthroughFuse
 
 
@@ -9,7 +11,8 @@ class GitFuse(PassthroughFuse):
     self.remote_url = remote_url
     self.root = self._get_root()
 
+    self.repo = Repository.clone(self.root, remote_url)
+
   def _get_root(self):
-    print self.remote_url
     match = re.search(r"(?P<repo_name>[A-Za-z0-9]+)\.git", self.remote_url)
     return "%s/%s" % (default_config.repos_path, match.group("repo_name"))
