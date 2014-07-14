dfb714807a0d82fd1dbd5c312da2f85ea5764a0e
vtemian <vladtemian@gmail.com>
2014-07-14 15:42:16 +0300

    Remove config from gitfs

--- gitfs/__init__.py
+++ gitfs/__init__.py
@@ -1,1 +1,1 @@
-from config import default_config
+
--- gitfs/filesystems/git.py
+++ gitfs/filesystems/git.py
@@ -1,21 +1,20 @@
 import re
 
-from gitiumFS import default_config
-from gitiumFS.utils import Repository
+from gitfs.utils import Repository
 
 from .passthrough import PassthroughFuse
 
 
 class GitFuse(PassthroughFuse):
-  def __init__(self, remote_url):
+  def __init__(self, remote_url, repos_path):
     self.remote_url = remote_url
-    self.root = self._get_root()
+    self.root = self._get_root(repos_path)
 
     self.repo = Repository.clone(remote_url, self.root)
 
-  def _get_root(self):
+  def _get_root(self, repos_path):
     match = re.search(r"(?P<repo_name>[A-Za-z0-9]+)\.git", self.remote_url)
-    return "%s/%s" % (default_config.repos_path, match.group("repo_name"))
+    return "%s/%s" % (repos_path, match.group("repo_name"))
 
   def write(self, path, buff, offset, fh):
     result = super(GitFuse, self).write(path, buff, offset, fh)
