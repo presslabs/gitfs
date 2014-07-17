305d83416aa71b44aaefb6d9f30a852088939cda
vtemian <vladtemian@gmail.com>
2014-07-17 13:27:14 +0300

    Added support for path building in view class

--- gitfs/views/index.py
+++ gitfs/views/index.py
@@ -1,3 +1,5 @@
+import os
+
 from fuse import Operations
 
 from .view import View
@@ -12,3 +14,18 @@ class IndexView(View, Operations):
 
     def open(self, path, argument):
         print path, argument
+
+    def build_paths(self, root=''):
+        """ Get all paths from repos. Hold them in memory.
+        """
+        full_path = self._full_path(self.repo_path, root)
+        paths = {'.': {}, '..': {}}
+
+        items = os.listdir(full_path)
+        for item in items:
+            if os.path.isdir("%s/%s" % (full_path, item)):
+                paths[item] = self.build_paths("%s%s/" % (root, item))
+            else:
+                paths[item] = {}
+
+        return paths
--- gitfs/views/view.py
+++ gitfs/views/view.py
@@ -1,6 +1,25 @@
+import os
+from abc import ABCMeta, abstractmethod
+
+
 class View(object):
+    __metaclass__ = ABCMeta
+
     def __init__(self, *args, **kwargs):
         self.args = args
 
         for attr in kwargs:
             setattr(self, attr, kwargs[attr])
+
+        self.paths = self.build_paths()
+
+    @abstractmethod
+    def build_paths(self, root=''):
+        pass
+
+    def _full_path(self, root, partial):
+        if partial.startswith("/"):
+            partial = partial[1:]
+        path = os.path.join(root, partial)
+
+        return path
