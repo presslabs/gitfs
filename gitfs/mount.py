2f18a179fdcaeb5c9b22fb7fb7cba73c5da6684d
Emanuel Danci <danci.emanuel@gmail.com>
2014-07-28 19:28:36 +0300

    dev mount settings

--- gitfs/mount.py
+++ gitfs/mount.py
@@ -1,9 +1,15 @@
 from router import Router
 from views import IndexView
 
+mount_path = '/tmp/gitfs/mnt'
 
-router = Router('~/presslabs/testing.git', '/tmp')
-router.register(r'^/complete/relativity/is/good/(?P<path>\w+)/(\w+)',
-                IndexView)
+router = Router('/home/zalman/dev/presslabs/test-repo.git', '/tmp/gitfs/repos/')
 
-router.open('/cpmplete/relativity/is/good/one/two/three/four/five.six', 'arg')
+#router.register(r'^/complete/relativity/is/good/(?P<path>\w+)/(\w+)',
+                #IndexView)
+
+#router.open('/complete/relativity/is/good/one/two/three/four/five.six', 'arg')
+router.register(r'^/', IndexView)
+
+from fuse import FUSE
+FUSE(router, mount_path, foreground=True, nonempty=True)
