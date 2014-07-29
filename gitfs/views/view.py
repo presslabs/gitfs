adf3a500e8ab8115520daa16bc008faeec7cfca9
Emanuel Danci <danci.emanuel@gmail.com>
2014-07-29 14:19:59 +0300

    Make View inherit from objects instead of PassthroughFuse

--- gitfs/views/view.py
+++ gitfs/views/view.py
@@ -2,9 +2,8 @@ import os
 from abc import ABCMeta, abstractmethod
 
 from gitfs import  FuseMethodNotImplemented
-from gitfs.filesystems.passthrough import PassthroughFuse
 
-class View(PassthroughFuse):
+class View(object):
     __metaclass__ = ABCMeta
 
     def __init__(self, *args, **kwargs):
@@ -12,7 +11,6 @@ class View(PassthroughFuse):
 
         for attr in kwargs:
             setattr(self, attr, kwargs[attr])
-
 
     def getxattr(self, path, name, position=0):
         """Get extended attributes"""
