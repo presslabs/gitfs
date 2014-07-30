09eb2a6e7d76011c9916a7882a62060f5f720d74
Calin Don <calin.don@gmail.com>
2014-07-30 20:52:13 +0300

    support for create/access symlinks

--- gitfs/views/current.py
+++ gitfs/views/current.py
@@ -1,6 +1,6 @@
 import re
+import os
 from .view import View
-from log import log
 from gitfs.filesystems.passthrough import PassthroughFuse
 
 
@@ -13,3 +13,9 @@ class CurrentView(View, PassthroughFuse):
     def rename(self, old, new):
         new = re.sub(self.regex, '', new)
         super(CurrentView, self).rename(old, new)
+
+    def symlink(self, name, target):
+        return os.symlink(target, self._full_path(name))
+
+    def readlink(self, path):
+        return os.readlink(self._full_path(path))
