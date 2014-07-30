3389c6208a86d4ec7ba9594e6f0f57f082d81882
Emanuel Danci <danci.emanuel@gmail.com>
2014-07-30 16:50:27 +0300

    Update HistoryIndeView - listdir is working.

--- gitfs/views/history_index.py
+++ gitfs/views/history_index.py
@@ -1,8 +1,10 @@
-from .view import View
 
+from datetime import datetime
 from errno import ENOENT
 from stat import S_IFDIR
+from pygit2 import GIT_SORT_TIME
 
+from .view import View
 from gitfs import  FuseMethodNotImplemented, FuseOSError
 from log import log
 
@@ -36,5 +38,29 @@ class HistoryIndexView(View):
         log.info('%s %s', path, amode)
         return 0
 
+    def _get_commit_history(self):
+        """
+        Walk through all commits from current repo in order to compose the
+        _history_ directory.
+        """
+        paths = {}
+
+        for commit in self.repo.walk(self.repo.head.target, GIT_SORT_TIME):
+            commit_time = datetime.fromtimestamp(commit.commit_time)
+
+            day = "%s-%s-%s" % (commit_time.year, commit_time.month,
+                                commit_time.day)
+            time = "%s-%s-%s" % (commit_time.hour, commit_time.minute,
+                                 commit_time.second)
+
+            paths[day] = "%s-%s" % (time, commit.hex[:7])
+            #paths[day] = "%s-%s" % (time, commit.hex)
+
+        return paths
+
     def readdir(self, path, fh):
-        return ['.', '..', 'commits everywhere']
+        commit_hist = self._get_commit_history()
+
+        dir_entries = ['.', '..'] + commit_hist.keys()
+        for entry in dir_entries:
+            yield entry
