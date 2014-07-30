5b819746a7cc768a461320c2e5de506bb477a641
Emanuel Danci <danci.emanuel@gmail.com>
2014-07-30 19:42:47 +0300

    Add regex for timestamp and sha.

--- gitfs/mount.py
+++ gitfs/mount.py
@@ -13,7 +13,8 @@ router = Router(remote_url='/home/zalman/dev/presslabs/test-repo.git',
 
 # TODO: replace regex with the strict one for the Historyview
 # -> r'^/history/(?<date>(19|20)\d\d[-](0[1-9]|1[012])[-](0[1-9]|[12][0-9]|3[01]))/',
-router.register(r'^/history/(?P<date>\d{4}-\d{1,2}-\d{1,2})', HistoryView)
+router.register(r'^/history/(?P<date>\d{4}-\d{1,2}-\d{1,2})/(?P<time>\d{2}:\d{2}:\d{2})-(?P<commit_sha1>[0-9a-f]{10})', HistoryView)
+router.register(r'^/history/(?P<date>\d{4}-\d{1,2}-\d{1,2})', HistoryIndexView)
 router.register(r'^/history', HistoryIndexView)
 router.register(r'^/current', CurrentView)
 router.register(r'^/', IndexView)
