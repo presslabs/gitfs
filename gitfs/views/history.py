78f6bda69c7cdcb52057971edc0853b0045aa31a
Emanuel Danci <danci.emanuel@gmail.com>
2014-07-31 16:27:40 +0300

    Add the possibility of browsing the tree of a particular commit.

--- gitfs/views/history.py
+++ gitfs/views/history.py
@@ -1,9 +1,7 @@
-from datetime import datetime
-from errno import ENOENT
+import os
 from stat import S_IFDIR
-from pygit2 import GIT_SORT_TIME
+from pygit2 import GIT_FILEMODE_TREE
 
-from gitfs import   FuseOSError
 from log import log
 from .view import View
 
@@ -34,6 +32,22 @@ class HistoryView(View):
         log.info('%s %s', path, amode)
         return 0
 
-    def readdir(self, path, fh):
-        return ['.', '..', 'gigi are mere']
+    def _get_commit_subtree(self, tree, subtree_name):
+        for e in tree:
+            if e.filemode == GIT_FILEMODE_TREE:
+                if e.name == subtree_name:
+                    return self.repo[e.id]
+                else:
+                    return self._get_commit_subtree(self.repo[e.id],
+                                                    subtree_name)
 
+    def readdir(self, path, fh):
+        dir_entries = ['.', '..']
+        commit = self.repo.revparse_single(self.commit_sha1)
+        if getattr(self, 'relative_path'):
+            tree_name = os.path.split(self.relative_path)[1]
+            subtree = self._get_commit_subtree(commit.tree, tree_name)
+            [dir_entries.append(entry.name) for entry in subtree]
+        else:
+            [dir_entries.append(entry.name) for entry in commit.tree]
+        return dir_entries
