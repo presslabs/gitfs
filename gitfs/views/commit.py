240741c8694a4aae9dc16b8dc61694836f5c231e
Emanuel Danci <danci.emanuel@gmail.com>
2014-08-04 18:11:28 +0300

    Set the commit to be an attribute + add comments.

--- gitfs/views/commit.py
+++ gitfs/views/commit.py
@@ -11,6 +11,10 @@ from .view import View
 
 class CommitView(View):
 
+    def __init__(self, *args, **kwargs):
+        super(CommitView, self).__init__(*args, **kwargs)
+        self.commit = self.repo.revparse_single(self.commit_sha1)
+
     def _get_git_object_type(self, tree, entry_name):
         for e in tree:
             if e.name == entry_name:
@@ -19,6 +23,15 @@ class CommitView(View):
                 return self._get_git_object_type(self.repo[e.id], entry_name)
 
     def _get_blob_content(self, tree, blob_name):
+        """
+        Returns the content of a Blob object with the name <blob_name>.
+
+        :param tree: a commit tree or a simple tree
+        :param blob_name: the name of the blob that is being searched for
+        :type blob_name: str
+        :returns: the content of the blob with the name <blob_name>
+        :rtype: str
+        """
         for node in tree:
             if node.name == blob_name:
                 return self.repo[node.id].data
@@ -27,9 +40,8 @@ class CommitView(View):
 
 
     def readlink(self, path):
-        commit = self.repo.revparse_single(self.commit_sha1)
         obj_name = os.path.split(path)[1]
-        return self._get_blob_content(commit.tree, obj_name)
+        return self._get_blob_content(self.commit.tree, obj_name)
 
     def getattr(self, path, fh=None):
         '''
@@ -43,12 +55,13 @@ class CommitView(View):
         the directory, while Linux counts only the subdirectories.
         '''
 
-        # TODO:
-        #   * treat blob and blob_executable differently
+        """
+        TODO:
+            * Use the rights provided at mount point.
+        """
         if path and path != '/':
-            commit = self.repo.revparse_single(self.commit_sha1)
             obj_name = os.path.split(path)[1]
-            obj_type = self._get_git_object_type(commit.tree, obj_name)
+            obj_type = self._get_git_object_type(self.commit.tree, obj_name)
             if obj_type == GIT_FILEMODE_LINK:
                 return dict(st_mode=(S_IFLNK | 0644))
             if obj_type == GIT_FILEMODE_BLOB or\
@@ -88,14 +101,13 @@ class CommitView(View):
 
     def _get_dir_entries(self, name):
         dir_entries = []
-        commit = self.repo.revparse_single(self.commit_sha1)
-        dir_tree = commit.tree
+        dir_tree = self.commit.tree
 
         # If the relative_path is not empty, fetch the git tree corresponding
         # to the directory that we are in.
         tree_name = os.path.split(self.relative_path)[1]
         if tree_name:
-            subtree = self._get_commit_subtree(commit.tree, tree_name)
+            subtree = self._get_commit_subtree(self.commit.tree, tree_name)
             dir_tree = subtree
 
         [dir_entries.append(entry) for entry in dir_tree]
@@ -104,14 +116,13 @@ class CommitView(View):
 
     def readdir(self, path, fh):
         dir_entries = ['.', '..']
-        commit = self.repo.revparse_single(self.commit_sha1)
-        dir_tree = commit.tree
+        dir_tree = self.commit.tree
 
         # If the relative_path is not empty, fetch the git tree corresponding
         # to the directory that we are in.
         tree_name = os.path.split(self.relative_path)[1]
         if tree_name:
-            subtree = self._get_commit_subtree(commit.tree, tree_name)
+            subtree = self._get_commit_subtree(self.commit.tree, tree_name)
             dir_tree = subtree
 
         [dir_entries.append(entry.name) for entry in dir_tree]
