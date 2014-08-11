def77d3a9ca545fee57d76cba28035fd94e50607
vtemian <vladtemian@gmail.com>
2014-08-11 19:14:26 +0300

    Update travis.sh

--- .travis.sh
+++ .travis.sh
@@ -5,6 +5,6 @@ sudo apt-get install -y udev fuse-utils libfuse-dev libfuse2 libgit2-0 libgit2-d
 
 sudo mknod /dev/fuse -m 0666 c 10 229
 sudo chown root:root /dev/fuse
-useradd -G fuse travis
+adduser travis group
 sudo chmod g+rw /dev/fuse
 exec su -l $USER
