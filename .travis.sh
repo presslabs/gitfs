3de70b7f7613d75a8d244a55815a4cc75a750605
vtemian <vladtemian@gmail.com>
2014-08-12 13:20:47 +0300

    He doesnt like 7777

--- .travis.sh
+++ .travis.sh
@@ -3,6 +3,6 @@ sudo add-apt-repository -y ppa:presslabs/testing-ppa
 sudo apt-get update
 sudo apt-get install -y udev fuse-utils libfuse-dev libfuse2 libgit2-0 libgit2-dev git git-core
 
-sudo mknod /dev/fuse -m 7777 c 10 229
+sudo mknod /dev/fuse -m 0777 c 10 229
 sudo chown travis:travis /dev/fuse
 sudo adduser travis fuse
