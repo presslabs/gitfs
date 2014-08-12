f374ac8bb3789ed533a2371eae78a9f98e1def60
vtemian <vladtemian@gmail.com>
2014-08-12 13:36:41 +0300

    Test file reading for current view

--- tests/integrations/current/test_read.py
+++ tests/integrations/current/test_read.py
@@ -6,3 +6,7 @@ from tests.integrations.base import BaseTest
 class TestReadCurrentView(BaseTest):
     def test_listdirs(self):
         assert os.listdir("%s/current" % self.mount_path) == ['testing', 'me']
+
+    def test_read_from_a_file(self):
+        with open("%s/current/testing" % self.mount_path) as f:
+            assert f.read() == "just testing around here\n"
