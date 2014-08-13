5e6eb3d6441991c983a98c671be1340f6fb06a9e
vtemian <vladtemian@gmail.com>
2014-08-13 12:28:06 +0300

    No more travis for you girl

--- .travis.sh
+++
@@ -1,8 +0,0 @@
-sudo apt-get install -y software-properties-common python-software-properties
-sudo add-apt-repository -y ppa:presslabs/testing-ppa
-sudo apt-get update
-sudo apt-get install -y udev fuse-utils libfuse-dev libfuse2 libgit2-0 libgit2-dev git git-core
-
-sudo mknod /dev/fuse -m 0777 c 10 229
-sudo chown travis:travis /dev/fuse
-sudo adduser travis fuse
