a92449dc331dbac0d331f5b1716017a6af285c0c
vtemian <vladtemian@gmail.com>
2014-09-04 21:31:36 +0300

    Refactor and add more docs for MergeWorker

--- gitfs/worker/merge.py
+++ gitfs/worker/merge.py
@@ -25,41 +25,66 @@ class MergeWorker(FetchWorker):
         while True:
             try:
                 job = self.merge_queue.get(timeout=self.timeout, block=True)
+
+                # we have two types of jobs: [commit, merge]
                 if job['type'] == 'commit':
                     commits.append(job)
                 elif job['type'] == 'merge':
                     merges.append(job)
             except:
-                if commits and merges:
-                    self.commit(commits)
-                    self.want_to_merge.set()
-                    commits = []
-                    merges = []
-                elif merges:
-                    self.want_to_merge.set()
-                    merges = []
-                elif commits:
-                    self.commit(commits)
-                    self.want_to_merge.set()
-                    commits = []
-                elif (self.want_to_merge.is_set() and
-                      not self.somebody_is_writing.is_set()):
-                    self.fetch()
-                    self.merge()
-                    self.want_to_merge.clear()
-                    self.push()
+                commits, merges = self.on_idle(commits, merges)
+
+    def on_idle(self, commits, merges):
+        """
+        On idle, we have 4 cases:
+        1. We have to commit and also need to merge some commits from remote.
+        In this case, we commit and announce ourself for merging
+        2. We are behind from remote, so we announce for merging
+        3. We only need to commit
+        4. We announced for merging and nobody is writing in this momement.
+        In this case we are safe to merge and push.
+        """
+
+        if commits and merges:
+            self.commit(commits)
+            self.want_to_merge.set()
+            commits = []
+            merges = []
+        elif merges:
+            self.want_to_merge.set()
+            merges = []
+        elif commits:
+            self.commit(commits)
+            self.want_to_merge.set()
+            commits = []
+        elif (self.want_to_merge.is_set() and
+              not self.somebody_is_writing.is_set()):
+            self.merge()
+            self.want_to_merge.clear()
+            self.push()
+
+        return commits, merges
 
     @while_not("read_only")
     def merge(self):
         self.strategy(self.branch, self.branch, self.upstream)
         self.repository.commits.update()
-        self.want_to_merge.clear()
 
     @retry(3)
     def push(self):
+        """
+        Try to push. The push can fail in two cases:
+        1. The remote is done, so we need to retry.
+        2. We are behind, so we need to fetch + merge and then try again
+        """
+
         self.read_only.set()
-        self.repository.push(self.upstream, self.branch)
-        self.read_only.clear()
+
+        try:
+            self.repository.push(self.upstream, self.branch)
+            self.read_only.clear()
+        except:
+            pass
 
     def commit(self, jobs):
         if len(jobs) == 1:
