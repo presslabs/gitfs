a0f3b298473df3babb586db121ab39c72cc5f11a
vtemian <vladtemian@gmail.com>
2014-09-12 18:41:34 +0300

    test: forbidden method for a read-only view

--- tests/views/test_read_only.py
+++ tests/views/test_read_only.py
@@ -1,2 +1,18 @@
+import pytest
+
+from fuse import FuseOSError
+
+from gitfs.views.read_only import ReadOnlyView
+
+
 class TestReadOnly(object):
-    pass
+    def test_cant_write(self):
+        view = ReadOnlyView()
+
+        for method in ["getxattr", "write", "create", "utimens",
+                       "chmod", "mkdir"]:
+            with pytest.raises(FuseOSError):
+                getattr(view, method)("path", 1)
+
+        with pytest.raises(FuseOSError):
+            view.chown("path", 1, 1)
