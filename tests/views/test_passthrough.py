b2935dbc9b01dadbe9395d6a46e6505043587821
vtemian <vladtemian@gmail.com>
2014-09-12 19:20:44 +0300

    test: access for passthrough

--- tests/views/test_passthrough.py
+++ tests/views/test_passthrough.py
@@ -1,4 +1,7 @@
+import pytest
 from mock import patch
+
+from fuse import FuseOSError
 
 from gitfs.views.passthrough import PassthroughView
 
@@ -8,7 +11,18 @@ class TestPassthrodugh(object):
         with patch('gitfs.views.passthrough.os') as mocked_os:
             mocked_os.path.join.return_value = "simple_path"
 
-        pass_view = PassthroughView(repo_path="/root")
+            pass_view = PassthroughView(repo_path="/root")
 
-        assert pass_view._full_path("/repo") == "/root/repo"
-        mocked_os.path.join.asserted_called_once_with("/root", "repo")
+            assert pass_view._full_path("/repo") == "simple_path"
+            mocked_os.path.join.asserted_called_once_with("/root", "repo")
+
+    def test_access(self):
+         with patch('gitfs.views.passthrough.os') as mocked_os:
+             mocked_os.access.return_value = False
+
+             pass_view = PassthroughView(repo_path="/root")
+
+             with pytest.raises(FuseOSError):
+                 pass_view.access("path", "mode")
+
+             mocked_os.access.asserted_called_once_with("/root/path", "mode")
