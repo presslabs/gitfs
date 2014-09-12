f8703d8763473b2e353c5dc721bf9d83f9371dfd
vtemian <vladtemian@gmail.com>
2014-09-12 19:36:39 +0300

    test: getattr with a correct path for IndexView class

--- tests/views/test_index.py
+++ tests/views/test_index.py
@@ -1,3 +1,5 @@
+from stat import S_IFDIR
+
 import pytest
 
 from fuse import FuseOSError
@@ -11,3 +13,21 @@ class TestIndexView(object):
 
         with pytest.raises(FuseOSError):
             view.getattr("/path")
+
+    def test_getattr_with_correct_path(self):
+        view = IndexView(**{
+            'uid': 1,
+            'gid': 1,
+            'mount_time': "now"
+        })
+        result = view.getattr("/", 1)
+
+        asserted_result = {
+            'st_mode': S_IFDIR | 0555,
+            'st_nlink': 2,
+            'st_uid': 1,
+            'st_gid': 1,
+            'st_ctime': "now",
+            'st_mtime': "now",
+        }
+        assert result == asserted_result
