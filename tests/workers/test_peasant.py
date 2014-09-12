b1f0bc0fdc1bc3004cf34183fc3b0da64d2cba22
vtemian <vladtemian@gmail.com>
2014-09-12 14:07:29 +0300

    cleanup: remove unused mock

--- tests/workers/test_peasant.py
+++ tests/workers/test_peasant.py
@@ -5,7 +5,7 @@ from gitfs.worker.peasant import Peasant
 
 class TestPeasant(object):
     def test_peasent_init(self):
-        with patch('gitfs.worker.peasant.Thread') as mocked_thread:
+        with patch('gitfs.worker.peasant.Thread'):
             peasant = Peasant(simple_attribute="value")
 
             assert peasant.simple_attribute == "value"
