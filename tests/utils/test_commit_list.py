1eff4498be5f702ab4f626e279e9ce6143c3d2e0
vtemian <vladtemian@gmail.com>
2014-09-16 18:53:58 +0300

    test: contains and index from custom commit list

---
+++ tests/utils/test_commit_list.py
@@ -0,0 +1,20 @@
+from mock import MagicMock
+
+from gitfs.utils.commits import CommitsList
+
+
+class TestCommitList(object):
+    def test_contains(self):
+        mocked_commit = MagicMock()
+        mocked_commit.hex = "hexish"
+
+        commit_list = CommitsList()
+        assert mocked_commit not in commit_list
+
+    def test_index(self):
+        mocked_commit = MagicMock()
+        mocked_commit.hex = "hexish"
+
+        commit_list = CommitsList()
+        commit_list.append(mocked_commit)
+        assert commit_list.index(mocked_commit) == 0
