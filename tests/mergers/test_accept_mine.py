31a8ebf3e6c9d35d9e5d88e8d1adfd5f5fabc213
vtemian <vladtemian@gmail.com>
2014-09-18 14:06:26 +0300

    test: solve conflicts when a file is missing

--- tests/mergers/test_accept_mine.py
+++ tests/mergers/test_accept_mine.py
@@ -1,4 +1,4 @@
-from mock import MagicMock
+from mock import MagicMock, patch
 
 from pygit2 import GIT_BRANCH_LOCAL
 
@@ -19,3 +19,43 @@ class TestAcceptMine(object):
         mocked_repo.lookup_branch.assert_called_once_with("old_branch",
                                                           GIT_BRANCH_LOCAL)
         mocked_branch.rename.assert_called_once_with("new_branch", True)
+
+    def test_reload_branch(self):
+        mocked_repo = MagicMock()
+        mocked_branch = MagicMock()
+
+        mocked_branch.rename.return_value = "branch"
+        mocked_branch.get_object.return_value = "remote_commit"
+        mocked_repo.lookup_branch.return_value = mocked_branch
+        mocked_repo.lookup_reference.return_value = "ref"
+
+        mine = AcceptMine(mocked_repo)
+        mine.reload_branch("branch", "origin")
+
+    def test_solve_conflicts_we_deleted_the_file(self):
+        mocked_repo = MagicMock()
+        mocked_file = MagicMock()
+
+        mocked_file.path = "simple_path"
+
+        def conflicts():
+            yield None, mocked_file, None
+
+        mine = AcceptMine(mocked_repo)
+        mine.solve_conflicts(conflicts())
+
+        mocked_repo.index.remove.assert_called_once_with("simple_path")
+
+    def test_solve_conflicts_they_deleted_the_file(self):
+        mocked_repo = MagicMock()
+        mocked_file = MagicMock()
+
+        mocked_file.path = "simple_path"
+
+        def conflicts():
+            yield None, None, mocked_file
+
+        mine = AcceptMine(mocked_repo)
+        mine.solve_conflicts(conflicts())
+
+        mocked_repo.index.add.assert_called_once_with("simple_path")
