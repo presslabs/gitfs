419dccc14ed7c8a83e403a2f81bb0e77e5ebe767
vtemian <vladtemian@gmail.com>
2014-09-19 16:08:30 +0300

    test: fix nodes tests

--- tests/cache/test_node.py
+++ tests/cache/test_node.py
@@ -3,7 +3,12 @@ from gitfs.cache.node import Node
 
 class TestNode(object):
     def test_node(self):
-        node = Node('prev', 'next', 'data')
+        node = Node()
+
+        node.prev = "prev"
+        node.next = "next"
+        node.data = "data"
+
         assert node.prev == 'prev'
         assert node.next == 'next'
         assert node.data == 'data'
