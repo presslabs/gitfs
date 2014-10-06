74ba824309987bf1c162f1f59eddbff9d66febd4
vtemian <vladtemian@gmail.com>
2014-10-06 11:55:40 +0300

    use event.wait

--- gitfs/utils/decorators/while_not.py
+++ gitfs/utils/decorators/while_not.py
@@ -1,27 +1,21 @@
-import time
 import threading
 from functools import wraps
 
 
 class while_not(object):
-    def __init__(self, event_name, retry=0.2):
-        self.event_name = event_name
-        self.retry = retry
+    def __init__(self, event):
+        self.event = event
 
     def __call__(self, f):
         @wraps(f)
         def decorated(obj, *args, **kwargs):
-            event = getattr(obj, self.event_name, None)
-            if not event:
-                raise ValueError("Except that %s to have %s" %
-                                 (obj.__class__.__name__, self.event_name))
-            if not isinstance(event, threading._Event):
+            if not self.event:
+                raise ValueError("Except that %s to not be None %s" %
+                                 obj.__class__.__name__)
+            if not isinstance(self.event, threading._Event):
                 raise TypeError("%s should be of type threading.Event" %
-                                self.event_name)
-            while event.is_set():
-                print "Wait to %s" % f.__name__
-                time.sleep(self.retry)
-
+                                self.event)
+            self.event.wait()
             return f(obj, *args, **kwargs)
 
         return decorated
