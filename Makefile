22afe099d02398b2a7552802dbac50280e42cda7
dragostis <dragostiselice@gmail.com>
2014-10-08 13:04:45 +0300

    Fixed remaining tests.

--- Makefile
+++ Makefile
@@ -62,7 +62,7 @@ testenv:
 
 test: testenv
 	$(VIRTUAL_ENV)/bin/pip install -e .
-	$(VIRTUAL_ENV)/bin/gitfs $(BARE_REPO) $(MNT_DIR) -o repos_path=$(REPO_DIR) & echo "$$!" > $(GITFS_PID)
+	$(VIRTUAL_ENV)/bin/gitfs $(BARE_REPO) $(MNT_DIR) -o repos_path=$(REPO_DIR),fetch_timeout=2,merge_timeout=2 & echo "$$!" > $(GITFS_PID)
 	sleep 2
 	MOUNT_PATH=$(MNT_DIR) REPO_PATH=$(REPO_DIR) REPO_NAME=$(REPO_NAME) REMOTE=$(REPO) $(VIRTUAL_ENV)/bin/py.test tests
 	kill -9 `cat $(GITFS_PID)`
--- tests/integrations/current/test_write.py
+++ tests/integrations/current/test_write.py
@@ -12,7 +12,7 @@ class TestWriteCurrentView(BaseTest):
         with open(filename, "w") as f:
             f.write(content)
 
-        time.sleep(6)
+        time.sleep(5)
 
         # check if the write was done correctly
         with open(filename) as f:
@@ -32,7 +32,7 @@ class TestWriteCurrentView(BaseTest):
 
         os.makedirs(directory)
 
-        time.sleep(6)
+        time.sleep(5)
 
         with pull(self.sh):
             # check if directory exists or not
@@ -50,14 +50,15 @@ class TestWriteCurrentView(BaseTest):
         filename = "%s/testing" % self.current_path
         os.chmod(filename, 0766)
 
-        time.sleep(6)
+        time.sleep(5)
 
-        # check if the right mode was set
-        stats = os.stat(filename)
-        assert stats.st_mode == 0100766
+        with pull(self.sh):
+            # check if the right mode was set
+            stats = os.stat(filename)
+            assert stats.st_mode == 0100766
 
-        # self.assert_new_commit()
-        self.assert_commit_message("Chmod to 0766 on /testing")
+            self.assert_new_commit()
+            self.assert_commit_message("Chmod to 0766 on /testing")
 
     def test_rename(self):
         old_filename = "%s/testing" % self.current_path
@@ -65,13 +66,13 @@ class TestWriteCurrentView(BaseTest):
 
         os.rename(old_filename, new_filename)
 
-        time.sleep(6)
+        time.sleep(5)
 
         with pull(self.sh):
             # check for new file
             assert os.path.exists(new_filename)
 
-            self.assert_new_commit(2)
+            self.assert_new_commit()
             self.assert_commit_message("Rename /testing to /new_testing")
 
     def test_fsync(self):
@@ -82,7 +83,7 @@ class TestWriteCurrentView(BaseTest):
             f.write(content)
             os.fsync(f.fileno())
 
-        time.sleep(6)
+        time.sleep(5)
 
         with pull(self.sh):
             self.assert_new_commit()
@@ -92,7 +93,7 @@ class TestWriteCurrentView(BaseTest):
         filename = "%s/new_empty_file" % self.current_path
         open(filename, "a").close()
 
-        time.sleep(6)
+        time.sleep(5)
 
         with pull(self.sh):
             self.assert_new_commit()
@@ -103,7 +104,7 @@ class TestWriteCurrentView(BaseTest):
         name = "%s/links" % self.current_path
         os.symlink(target, name)
 
-        time.sleep(6)
+        time.sleep(5)
 
         with pull(self.sh):
             # check if link exists
