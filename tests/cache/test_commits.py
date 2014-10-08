8a76fafbade2795c6411ab9dcb82c6ca6b9fca03
vtemian <vladtemian@gmail.com>
2014-10-08 14:23:05 +0300

    fix tests

--- tests/cache/test_commits.py
+++ tests/cache/test_commits.py
@@ -31,10 +31,10 @@ class TestCommitCache(object):
 
         cache['2014-09-20'] = Commit(1, 1, "1111111111")
         assert cache.keys() == ['2014-09-20', '2014-09-19']
-        asserted_time = datetime.utcfromtimestamp(mocked_commit.commit_time)
+        asserted_time = datetime.fromtimestamp(mocked_commit.commit_time)
         asserted_time = "%s:%s:%s" % (asserted_time.hour, asserted_time.minute,
                                       asserted_time.second)
-        assert repr(cache['2014-09-19']) == '[16:56:40-1111111111]'
+        assert repr(cache['2014-09-19']) == '[%s-1111111111]' % asserted_time
         del cache['2014-09-20']
         for commit_date in cache:
             assert commit_date == '2014-09-19'
