84197b1594208979efc71130ed9b8d806ce17ee1
Calin Don <calin.don@gmail.com>
2014-10-14 16:00:59 +0300

    moved atomiclong as separate dependency

--- setup.py
+++ setup.py
@@ -1,27 +1,7 @@
 from setuptools import setup, find_packages
 from distutils.command.build import build
 
-requires_list = [
-    'fusepy==2.0.2',
-    'pygit2==0.21.3',
-]
-
-class CFFIBuild(build):
-    """Hack to combat the chicken and egg problem that we need cffi
-    to add cffi as an extension.
-    """
-    def finalize_options(self):
-        # This ffi is pygit2.ffi due to the path trick used in the beginning
-        # of the file
-        from gitfs.utils import atomic
-
-        self.distribution.ext_modules.append(atomic.ffi.verifier.get_extension())
-        build.finalize_options(self)
-
-
-cmdclass = {
-    'build': CFFIBuild
-}
+REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
 
 setup(name='gitfs',
       version='0.1.0',
@@ -34,10 +14,7 @@ setup(name='gitfs',
       entry_points={'console_scripts': ['gitfs = gitfs:mount']},
       zip_safe=False,
       include_package_data=True,
-      setup_requires=['cffi'] + requires_list,
-      install_requires=requires_list,
-      ext_modules=[],
-      cmdclass=cmdclass,
+      install_requires=REQUIREMENTS,
       classifiers=[
           'Programming Language :: Python :: 2.7',
       ])
