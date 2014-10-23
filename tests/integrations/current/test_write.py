1000550b1e4d78417d2cf0b268a1fa5640f3ce46
vtemian <vladtemian@gmail.com>
2014-10-23 17:55:07 +0300

    test: rmdir for more than one level deph

--- tests/integrations/current/test_write.py
+++ tests/integrations/current/test_write.py
@@ -17,9 +17,9 @@ import os
 import time
 import pytest
 import string
+import shutil
 
 from tests.integrations.base import BaseTest, pull
-from fuse import FuseOSError
 
 
 class TestWriteCurrentView(BaseTest):
@@ -325,3 +325,22 @@ class TestWriteCurrentView(BaseTest):
 
         with pull(self.sh):
             self.assert_commit_message("Deleted /deletable_file")
+
+    def test_delete_a_directory(self):
+        path = "%s/a_directory/another_dir/" % self.current_path
+        os.makedirs(path)
+
+        time.sleep(5)
+
+        with pull(self.sh):
+            self.assert_new_commit()
+
+        shutil.rmtree("%s/a_directory/" % self.current_path)
+
+        time.sleep(5)
+
+        with pull(self.sh):
+            self.assert_commit_message("Update 2 items")
+            self.assert_new_commit()
+
+        assert os.path.exists(path) is False
