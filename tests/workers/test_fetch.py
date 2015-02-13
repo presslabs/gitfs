7ef4562187a3e7539549c23d1df5a63a6e7392dd
vtemian <vladtemian@gmail.com>
2015-02-13 13:49:33 +0200

    test: when idle mode is active, fetch timeout should be greater than normal timeout

--- tests/workers/test_fetch.py
+++ tests/workers/test_fetch.py
@@ -56,3 +56,24 @@ class TestFetchWorker(object):
             mocked_repo.fetch.assert_called_once_with("origin", "master")
             assert mocked_fetch_ok.clear.call_count == 1
             assert mocked_fetch.clear.call_count == 1
+
+    def test_fetch_in_idle_mode(self):
+        mocked_peasant = MagicMock()
+        mocked_fetch = MagicMock(side_effect=ValueError)
+        mocked_fetch_event = MagicMock()
+        mocked_idle = MagicMock()
+
+        mocked_idle.is_set.return_value = True
+
+        with patch.multiple('gitfs.worker.fetch', Peasant=mocked_peasant,
+                            fetch=mocked_fetch_event, idle=mocked_idle):
+            worker = FetchWorker()
+            worker.fetch = mocked_fetch
+            worker.timeout = 5
+            worker.idle_timeout = 20
+
+            with pytest.raises(ValueError):
+                worker.work()
+
+            assert mocked_fetch.call_count == 1
+            mocked_fetch_event.wait.assert_called_once_with(20)
