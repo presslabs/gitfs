ac9fd85a082d1eeb7b86397fbe703694ddf0f478
Radu Ciorba <raduciorba@gmail.com>
2015-11-03 18:05:58 +0200

    start gitfs with more aggressive timings to speed up the tests

--- test.sh
+++ test.sh
@@ -28,7 +28,8 @@ cd test/origin.git;\
 echo "----------------------------"
 cd $ROOT
 
-gitfs test/origin.git test/mount_point -o repo_path=test/gitfs_owned_repo,fetch_timeout=2,merge_timeout=2,allow_other=true,log=log.txt,debug=true,idle_fetch_timeout=2
+TIMEOUT=0.2
+gitfs test/origin.git test/mount_point -o repo_path=test/gitfs_owned_repo,fetch_timeout=$TIMEOUT,merge_timeout=$TIMEOUT,allow_other=true,log=log.txt,debug=true,idle_fetch_timeout=$TIMEOUT
 
 MOUNT_PATH=test/mount_point REPO_PATH=test/gitfs_owned_repo REPO_NAME=dev_repo REMOTE=test/dev_repo py.test --capture=no
 pkill -f "gitfs test/origin.git"
